b6d5bc5037d0777dff8a65fa8607859708ac1d9c
B.J. Allen <bjallen@mac.com>
2012-07-17 20:29:34 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BjaThings::Application.config.secret_token = 'd01aea1bca8ec460894ae0cb62e19b8184a72131c5c9f12c70d5725e3f9662c094934dc9fe4e403f9b3739d4ceb01fdb6f97a596424b79cec5d2f17e3ee5e025'
